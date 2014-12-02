f9e18a4958da6a37a0b1bcd64d969e14598b815c
EmiPatoR <emipator@gmail.com>
2014-12-02 19:18:39 +0100

    First commit

---
+++ FFT/src/main.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : FFT.cpp
+// Author      : HRUSTIC Emir
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <complex>
+using namespace std;
+
+const double PI = 3.141592653589793238463;
+const float  PI_F = 3.14159265358979f;
+
+int main() {
+
+	complex<double> racineUnite(cos(2*PI/3),sin(2*PI/3));
+
+	cout << "Racine unite :" << pow(racineUnite,3) << endl; // prints
+	return 0;
+}
